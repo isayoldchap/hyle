0b04599db91c8c8a75534a30925753bcdb3dbcdc
steven richardson <stevenrichardson@stevens-MacBook-Air-2.local>
2020-02-02 17:41:20 -0600

    Remove checked in dist folder

---
+++ test/scorer.test.js
@@ -0,0 +1,17 @@
+import { scoreCombo, scoreSingle } from "../src/util/scorer.js";
+import { assert } from "chai";
+
+describe("A scorer", () => {
+  it("should score individual scores properly", () => {
+    assert.equal(scoreSingle("XXX"), 3);
+    assert.equal(scoreSingle("OOOO"), 4);
+    assert.equal(scoreSingle("XXXXX"), 5);
+  });
+
+  it("should score combinations properly", () => {
+    assert.equal(scoreCombo("XXX"), 7);
+    assert.equal(scoreCombo("XXOXX"), 12);
+    assert.equal(scoreCombo("OOOO"), 16);
+    assert.equal(scoreCombo("XXXXX"), 30);
+  });
+});
