60eaa71dfa003258b7dd6b95c95cf4a3d1587bd0
Rick Kippert <rkippert@att.net>
2020-02-20 22:24:19 -0600

    Move <Screen /> styles to separate css file

--- src/components/screen/screen.js
+++ src/components/screen/screen.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './screen.css';
 
 export const Screen = props => {
   const { children } = props;
 
-  return (
-    <div
-      style={{
-        background: '#e9e9e9',
-        display: 'flex',
-        flexDirection: 'column',
-        left: 0,
-        minHeight: '100%',
-        overflow: 'auto',
-        position: 'fixed',
-        top: 0,
-        width: '100%',
-      }}
-    >
-      {children}
-    </div>
-  );
+  return <div className="screen-wrapper">{children}</div>;
 };
 
 Screen.propTypes = {
