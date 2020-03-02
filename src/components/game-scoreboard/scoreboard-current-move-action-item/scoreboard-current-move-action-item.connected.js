ecee8a27979cc0794f33bbee4102961d8dba0181
Rick Kippert <rkippert@att.net>
2020-03-01 19:03:03 -0600

    Remove unused props from <ConnectedScoreboardCurrentMoveActionItem />

--- src/components/game-scoreboard/scoreboard-current-move-action-item/scoreboard-current-move-action-item.connected.js
+++ src/components/game-scoreboard/scoreboard-current-move-action-item/scoreboard-current-move-action-item.connected.js
@@ -1,39 +1,19 @@
 import { ScoreboardCurrentMoveActionItem } from './scoreboard-current-move-action-item';
 import { connect } from 'react-redux';
-import {
-  selectIsEndOfRound,
-  selectIsGameOver,
-  selectRoundNumber,
-  selectTurn,
-  selectWinning,
-  selectNextTile,
-  selectPlayer1Name,
-  selectPlayer1Score,
-  selectPlayer2Name,
-  selectPlayer2Score
-} from '../../../selectors/gameSelector';
-import { advanceRound, newGame, handlePass } from '../../../ducks/entropyDuck';
+import { selectIsEndOfRound, selectIsGameOver, selectNextTile, selectTurn } from '../../../selectors/gameSelector';
+import { handlePass } from '../../../ducks/entropyDuck';
 
 export const mapStateToProps = state => {
   return {
     endOfGame: selectIsGameOver(state),
     endOfRound: selectIsEndOfRound(state),
     nextTile: selectNextTile(state),
-    orderHalfMove: state.orderHalfMove,
-    player1Name: selectPlayer1Name(state),
-    player1Score: selectPlayer1Score(state),
-    player2Name: selectPlayer2Name(state),
-    player2Score: selectPlayer2Score(state),
-    roundNumber: selectRoundNumber(state),
-    turn: selectTurn(state),
-    winner: selectWinning(state)
+    turn: selectTurn(state)
   };
 };
 
 const mapDispatchToProps = {
-  pass: handlePass,
-  startNewGame: newGame,
-  startNextRound: advanceRound
+  pass: handlePass
 };
 
 export const ConnectedScoreboardCurrentMoveActionItem = connect(
