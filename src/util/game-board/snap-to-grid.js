6866285619966eda9e8e2b73d342f30975b9ef56
Rick Kippert <rkippert@att.net>
2020-03-01 18:02:37 -0600

    Remove obsolete util

--- src/util/game-board/snap-to-grid.js
+++
@@ -1,5 +0,0 @@
-export const snapToGrid = (x, y, cellSize = 32) => {
-  const snappedX = Math.round(x / cellSize) * cellSize;
-  const snappedY = Math.round(y / cellSize) * cellSize;
-  return [snappedX, snappedY];
-};
